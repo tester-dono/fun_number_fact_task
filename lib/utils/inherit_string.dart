401f470c2d13e02c8234f195146aa69d0274522a
shegida <den.shegida@gmail.com>
2022-05-11 13:29:15 +0300

    feat: created class and put it in inherit

--- lib/utils/inherit_string.dart
+++ lib/utils/inherit_string.dart
@@ -1,100 +1,81 @@
 import 'package:flutter/widgets.dart';
 
-class InheritString extends InheritedWidget {
-  const InheritString({
-    Key? key,
-    required this.string,
-    required Widget child,
-  }) : super(key: key, child: child);
-
-  final String string;
-
-  static InheritString of(BuildContext context) {
-    final InheritString? result = context.dependOnInheritedWidgetOfExactType<InheritString>();
-    assert(result != null, 'No FrogColor found in context');
-    return result!;
-  }
-
-  @override
-  bool updateShouldNotify(InheritString oldWidget) => string != oldWidget.string;
-}
-
 class StateWidget extends StatefulWidget {
   final Widget child;
 
   const StateWidget({
-    required this.child,
-  }) ;
+    required final this.child,
+  });
 
   @override
   _StateWidgetState createState() => _StateWidgetState();
 }
 
 class _StateWidgetState extends State<StateWidget> {
-  CoreState state = CoreState();
+  CoreState state = CoreState(klass: VeryDifficultClass(counter: 0));
 
   void incrementCounter() {
-    final counter = state.counter + 1;
-    final newState = state.copy(counter: counter);
-
-    setState(() => state = newState);
-  }
-
-  void setCounter(int counter) {
-    final newState = state.copy(counter: counter);
+    final klass = state.klass;
+    klass.counter++;
+    final newState = state.copy(klass: klass);
 
     setState(() => state = newState);
   }
 
   @override
   Widget build(BuildContext context) => StateInheritedWidget(
-    child: widget.child,
-    state: state,
-    stateWidget: this,
-  );
+        child: widget.child,
+        state: state,
+        stateWidget: this,
+      );
 }
 
 class StateInheritedWidget extends InheritedWidget {
   final CoreState state;
   final _StateWidgetState stateWidget;
 
-  StateInheritedWidget({
+  const StateInheritedWidget({
     required Widget child,
     required this.state,
     required this.stateWidget,
-  }) : super(child: child);
+  }) : super(child: child,);
 
   static _StateWidgetState of(BuildContext context) => context
-      .dependOnInheritedWidgetOfExactType<StateInheritedWidget>()
-      !.stateWidget;
+      .dependOnInheritedWidgetOfExactType<StateInheritedWidget>()!
+      .stateWidget;
 
   @override
   bool updateShouldNotify(StateInheritedWidget oldWidget) =>
       oldWidget.state != state;
 }
 
-
 class CoreState {
-  final int counter;
+  final VeryDifficultClass klass;
 
-  const CoreState({
-    this.counter = 0,
+  CoreState({
+    required this.klass,
   });
 
   CoreState copy({
-    required int counter,
+    required VeryDifficultClass klass,
   }) =>
       CoreState(
-        counter: counter,
+        klass: klass,
       );
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-          other is CoreState &&
-              runtimeType == other.runtimeType &&
-              counter == other.counter ;
+      other is CoreState &&
+          runtimeType == other.runtimeType &&
+          klass.counter == other.klass.counter;
 
   @override
-  int get hashCode => counter.hashCode;
+  int get hashCode => klass.counter.hashCode;
+}
+
+class VeryDifficultClass {
+  int counter;
+
+  VeryDifficultClass({required this.counter});
 }
