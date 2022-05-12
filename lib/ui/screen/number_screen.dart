81c34670ced577e672620b61fbea7ffe40209bb1
shegida <den.shegida@gmail.com>
2022-05-12 19:25:30 +0300

    feat: show i10n

--- lib/ui/screen/number_screen.dart
+++ lib/ui/screen/number_screen.dart
@@ -14,95 +14,134 @@ class NumberScreen extends ElementaryWidget<INumberWidgetModel> {
   @override
   Widget build(INumberWidgetModel wm) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(Strings.title),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          // возможно стоило разделить это в виджеты или приватные виджеты
+        appBar: AppBar(
+          title: const Text(Strings.title),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            // возможно стоило разделить это в виджеты или приватные виджеты
+            child: Column(
+              children: [
+                StateNotifierBuilder<String>(
+                  listenableState: wm.titleState,
+                  builder: (ctx, value) {
+                    return Text(value ?? Strings.errorCheck);
+                  },
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextField(
+                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        controller: wm.controller,
+                      ),
+                      EntityStateNotifierBuilder<String>(
+                          listenableEntityState: wm.factState,
+                          loadingBuilder: (_, data) {
+                            return const CircularProgressIndicator();
+                          },
+                          builder: (context, text) {
+                            return Text(text ?? Strings.errorCheck);
+                          }),
+                      const Divider(
+                        color: Colors.black,
+                      ),
+                      const Text(
+                        Strings.secondTitle,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      EntityStateNotifierBuilder<String>(
+                          listenableEntityState: wm.quizState,
+                          loadingBuilder: (_, data) {
+                            return const CircularProgressIndicator();
+                          },
+                          builder: (context, text) {
+                            return Text(text ?? Strings.errorCheck);
+                          }),
+                      EntityStateNotifierBuilder<String>(
+                          listenableEntityState: wm.fishState,
+                          loadingBuilder: (_, data) {
+                            return const CircularProgressIndicator();
+                          },
+                          builder: (context, text) {
+                            return Text(text ?? Strings.errorCheck);
+                          }),
+                      const Divider(
+                        color: Colors.black,
+                      ),
+                      EntityStateNotifierBuilder<String>(
+                          listenableEntityState: wm.launchState,
+                          loadingBuilder: (_, data) {
+                            return const CircularProgressIndicator();
+                          },
+                          builder: (context, text) {
+                            return Text(text ?? Strings.errorCheck);
+                          }),
+                      const Divider(
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                controller: wm.controller,
+              StateNotifierBuilder<String>(
+                listenableState: wm.genderState,
+                builder: (ctx, value) {
+                  return _FloatingNumberScreenButton(
+                    onPressed: wm.changeSex,
+                    iconData: value! == "female" ? Icons.woman : Icons.man,
+                  );
+                },
+              ),
+              StateNotifierBuilder<String>(
+                listenableState: wm.pluralState,
+                builder: (ctx, value) {
+                  return _FloatingNumberScreenButton(
+                    onPressed: wm.changeAmount,
+                    iconData: value! == "two" ? Icons.person : Icons.people,
+                  );
+                },
               ),
               EntityStateNotifierBuilder<String>(
-                  listenableEntityState: wm.factState,
-                  loadingBuilder: (_, data) {
-                    return const CircularProgressIndicator();
-                  },
-                  builder: (context, text) {
-                    return Text(text ?? Strings.errorCheck);
-                  }),
-              const Divider(
-                color: Colors.black,
-              ),
-              const Text(
-                Strings.secondTitle,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              EntityStateNotifierBuilder<String>(
-                  listenableEntityState: wm.quizState,
-                  loadingBuilder: (_, data) {
-                    return const CircularProgressIndicator();
-                  },
-                  builder: (context, text) {
-                    return Text(text ?? Strings.errorCheck);
-                  }),
-              EntityStateNotifierBuilder<String>(
-                  listenableEntityState: wm.fishState,
-                  loadingBuilder: (_, data) {
-                    return const CircularProgressIndicator();
-                  },
-                  builder: (context, text) {
-                    return Text(text ?? Strings.errorCheck);
-                  }),
-              const Divider(
-                color: Colors.black,
-              ),
-              EntityStateNotifierBuilder<String>(
-                  listenableEntityState: wm.launchState,
-                  loadingBuilder: (_, data) {
-                    return const CircularProgressIndicator();
-                  },
-                  builder: (context, text) {
-                    return Text(text ?? Strings.errorCheck);
-                  }),
-              const Divider(
-                color: Colors.black,
+                listenableEntityState: wm.factState,
+                loadingBuilder: (_, data) {
+                  return const _FloatingNumberScreenButton(
+                    iconData: Icons.sync_problem,
+                  );
+                },
+                builder: (_, data) {
+                  return _FloatingNumberScreenButton(
+                    onPressed: wm.sendRequest,
+                    iconData: Icons.add,
+                  );
+                },
               ),
             ],
           ),
-        ),
-      ),
-      floatingActionButton: EntityStateNotifierBuilder<String>(
-        listenableEntityState: wm.factState,
-        loadingBuilder: (_, data) {
-          return const _SendRequestButton(
-            iconData: Icons.sync_problem,
-          );
-        },
-        builder: (_, data) {
-          return _SendRequestButton(
-            onPressed: wm.sendRequest,
-            iconData: Icons.add,
-          );
-        },
-      ),
-    );
+        ));
   }
 }
 
-class _SendRequestButton extends StatelessWidget {
+class _FloatingNumberScreenButton extends StatelessWidget {
   final IconData iconData;
   final VoidCallback? onPressed;
 
-  const _SendRequestButton({
+  const _FloatingNumberScreenButton({
     Key? key,
     required this.iconData,
     this.onPressed,
